21f8996d744e918ddecd513a2c596f32e654f092
ajoanny <ajoanny@octo.com>
2026-03-17 22:06:25 +0100

    feat(direct): init implementation

--- test/golden-master.spec.ts
+++ test/golden-master.spec.ts
@@ -1,8 +1,28 @@
 import { Item, GildedRose } from '@/golden-master';
+import { GildedRose as GildedRoseAI } from '@/gilded-rose.ai.1';
+
 
 describe('Gilded Rose Golden master', () => {
   it('should pass', () => {
-    const items = [
+    const itemsOriginal = [
+      new Item("+5 Dexterity Vest", 10, 20),
+      new Item("Aged Brie", 2, 0),
+      new Item("Aged Brie", 0, 0),
+      new Item("Elixir of the Mongoose", 5, 7),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 0),
+      new Item("Backstage passes to a TAFKAL80ETC concert",0, 50),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 0),
+      new Item("Potion", 0, 5),
+      new Item("Potion", 0, 51),
+      new Item("Potion", 0, -1),
+    ];
+
+    const itemsAI = [
       new Item("+5 Dexterity Vest", 10, 20),
       new Item("Aged Brie", 2, 0),
       new Item("Aged Brie", 0, 0),
@@ -21,13 +41,13 @@ describe('Gilded Rose Golden master', () => {
     ];
 
 
-    const gildedRose = new GildedRose(items);
-
-    gildedRose.updateQuality();
+    const gildedRose = new GildedRose(itemsOriginal);
+    const gildedRoseAI = new GildedRoseAI(itemsAI);
 
     for(let i = 0; i<100; i++) {
-      const originalImpl = gildedRose.updateQuality();
-      expect(originalImpl).toBe(originalImpl);
+      gildedRose.updateQuality();
+      gildedRoseAI.updateQuality();
+      expect(itemsAI).toEqual(itemsOriginal);
     }
   });
 });
